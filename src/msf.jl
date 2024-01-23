7e248f19055193a3ce7723429b5398d95fec5cef
Miguel-Escobar <77211681+Miguel-Escobar@users.noreply.github.com>
2024-01-23 14:02:17 -0300

    quick optimization in msf.jl

--- src/msf.jl
+++ src/msf.jl
@@ -6,8 +6,6 @@ using GLMakie
 using ProgressMeter
 using Base.Threads
 using Roots
-using Plots
-
 
 function fhn_eom(x, params, t)
     a = params[1]
@@ -23,28 +21,30 @@ function fhn_jac(x, params, t)
     dx_dy = -1/eps
     dy_dx = 1
     dy_dy = 0
-    returnable = SA_F64[dx_dx dx_dy; dy_dx dy_dy] # Revisar con más detalle
+    returnable = SMatrix{2, 2, Float64}(dx_dx, dy_dx, dx_dy, dy_dy)  #SA_F64[dx_dx dx_dy; dy_dx dy_dy] 
     return returnable
 end
 
 function msf_eom!(dxchi, xchi, params, t)
-    aeps = params[1:2]
+    aeps = @view params[1:2]
     alpha = params[3]
     beta = params[4]
     c = params[5] 
     phi = params[6]
     B = couplingJacobian(phi, aeps[2])
-    chireal = xchi[3:4]
-    chiimag = xchi[5:6]
-    jac = fhn_jac(xchi[1:2], aeps, t)
-    dxchi[1:2] = fhn_eom(xchi[1:2], aeps, t)
-    dxchi[3:4] = jac * chireal .+ c.*(alpha.*B*chireal .- beta.*B*chiimag)
-    dxchi[5:6] = jac * chiimag .+ c.*(alpha.*B*chiimag .+ beta.*B*chireal)
+    chireal = @view xchi[3:4]
+    chiimag = @view xchi[5:6]
+    x = @view xchi[1:2]
+    jac = fhn_jac(x, aeps, t)
+    dxchi[1:2] .= fhn_eom(x, aeps, t)
+    dxchi[3:4] .= jac * chireal .+ c.*(alpha.*B*chireal .- beta.*B*chiimag)
+    dxchi[5:6] .= jac * chiimag .+ c.*(alpha.*B*chiimag .+ beta.*B*chireal)
     return nothing
 end
 
 function couplingJacobian(phi, eps)
-    return -1 .* SA[cos(phi)/eps sin(phi)/eps; -sin(phi) cos(phi)]
+    #return -1 .* SA[cos(phi)/eps sin(phi)/eps; -sin(phi) cos(phi)]
+    return -1 .* SMatrix{2, 2, Float64}(cos(phi)/eps, -sin(phi), sin(phi)/eps, cos(phi))
 end
 
 function msf_system(alpha, beta; a=0.5, eps=0.05, coupling=1.0, phi=(pi/2)-0.1, diffeq=(alg=Tsit5(), abstol = 1e-9, reltol = 1e-9))
@@ -73,15 +73,10 @@ function plot_msf_regions(n_rows; kwargs...)
     levels = [-1e10, 0, 1e10]
     println(alpha_sweep)
     println(beta_sweep)
-    p = Plots.contour(alpha_sweep, beta_sweep, msf;
-                levels=levels,
-                fill=true,
-                xlabel=L"α",
-                ylabel=L"β",
-                zlabel=L"λ",
-                cbar=false
-                )
-    display(p)
+    fig = Figure(resolution = (800, 600))
+    ax = Axis(fig, xlabel=L"α", ylabel=L"β", zlabel=L"λ")
+    contour!(ax, alpha_sweep, beta_sweep, msf, levels=levels, fill=true)
+    display(fig)
 end
 
 function plot_msf_vs_eigs(start, stop, n_points; kwargs...)
@@ -91,8 +86,10 @@ function plot_msf_vs_eigs(start, stop, n_points; kwargs...)
     Threads.@threads for i in 1:length(eigenvalue_real_sweep)
         msf_sweep[i] = master_stability_function(eigenvalue_real_sweep[i], 0.0; kwargs...)
     end
-    p = GLMakie.lines(eigenvalue_real_sweep, msf_sweep)
-    GLMakie.display(p)
+    fig = Figure(resolution = (800, 600))
+    ax = Axis(fig)
+    lines!(ax, eigenvalue_real_sweep, msf_sweep)
+    display(fig)
 end
 
 function synch_is_stable(coupling_matrix; kwargs...)
@@ -119,22 +116,16 @@ function plot_msf_regions_with_eigs(n_rows, coupling_matrix; savefigure=false, k
     msf_for_eigs = master_stability_function.(real.(eigs), imag.(eigs); kwargs...)
     levels = [-1e10, 0, 1e10]
 
-    p = Plots.contour(alpha_sweep, beta_sweep, msf;
-                levels=levels,
-                fill=true,
-                xlabel=L"α",
-                ylabel=L"β",
-                zlabel=L"λ",
-                cbar=false,
-                xlims=(-1.5, 0.5),
-                ylims=(-0.5, 0.5)
-                )
-    Plots.plot!(p, real.(eigs), imag.(eigs), seriestype=:scatter, color=:red, label="Eigenvalues", dpi=400, aspect_ratio=2)
+    fig = Figure(resolution = (800, 600))
+    ax = Axis(fig, xlabel=L"α", ylabel=L"β", zlabel=L"λ", xlims=(-1.5, 0.5), ylims=(-0.5, 0.5))
+    contour!(ax, alpha_sweep, beta_sweep, msf, levels=levels, fill=true)
+    scatter!(ax, real.(eigs), imag.(eigs), color=:red, label="Eigenvalues")
+    display(fig)
 
     if savefigure
-        Plots.savefig(p, "msf_with_eigs.png")
+        save("msf_with_eigs.png", fig)
     end
-    display(p)
+
     if all(msf_for_eigs .≤ 0)
         println("Synchronization is stable")
         println("Max Lyapunov: ", maximum(msf_for_eigs))
@@ -147,5 +138,3 @@ end
 function msf_zero(kwargs...)
     return find_zero(alpha -> master_stability_function(alpha, 0.0; kwargs...), 0.2)
 end
-
-    
